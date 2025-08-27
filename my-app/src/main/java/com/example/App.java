95359f3921f6c6a83a7cfa309765ee183cf6d086
Super User <root@ip-172-31-16-184.ap-southeast-2.compute.internal>
2025-08-27 03:59:24 +0000

    Initial Maven Java project

---
+++ my-app/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java 11!");
+        logger.info("Application started successfully.");
+    }
+}
+
